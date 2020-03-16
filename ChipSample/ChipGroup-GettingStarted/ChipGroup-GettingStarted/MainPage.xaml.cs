d6ca8eec38797bbead8b75d0eec3658044b722c7
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-03-16 14:14:25 +0530

    Demo sample added

---
+++ ChipSample/ChipGroup-GettingStarted/ChipGroup-GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Syncfusion.XForms.Buttons;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ChipGroup_GettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SessionListFilterOptions_SelectionChanged(object sender, Syncfusion.Buttons.XForms.SfChip.SelectionChangedEventArgs e)
+        {
+            if (e.AddedItem != null)
+            {
+                (e.AddedItem as Language).IsChecked = true;
+            }
+
+            if (e.RemovedItem != null)
+            {
+                (e.RemovedItem as Language).IsChecked = false;
+            }
+        }
+    }
+}
