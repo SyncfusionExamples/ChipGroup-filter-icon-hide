b38efd6541e15878fc15d8ac5abfb35486a324be
Mohanlal Gopu <mohanlal.gopu@syncfusion.com>
2021-01-18 00:03:38 +0530

    1 . Update new version.

---
+++ ChipGroup_GettingStarted/ChipGroup_GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChipGroup_GettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SessionListFilterOptions_SelectionChanged(object sender, Syncfusion.Buttons.XForms.SfChip.SelectionChangedEventArgs e)
+        {
+            if (e.AddedItem != null)
+            {
+                (e.AddedItem as Language).IsChecked = true;
+            }
+
+            if (e.RemovedItem != null)
+            {
+                (e.RemovedItem as Language).IsChecked = false;
+            }
+        }
+
+    }
+}
---
+++ ChipGroup_GettingStarted/ChipGroup_GettingStarted/Model.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ChipGroup_GettingStarted
+{
+    public class Language : INotifyPropertyChanged
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        private Color textColor = Color.White;
+
+
+
+        public Color TextColor
+        {
+            get { return textColor; }
+            set
+            {
+                textColor = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TextColor"));
+            }
+        }
+
+        private bool isChecked = false;
+
+
+
+        public bool IsChecked
+        {
+            get { return isChecked; }
+            set
+            {
+                isChecked = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsChecked"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
